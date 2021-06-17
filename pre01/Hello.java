25d65956f51eb31c8f01700b1e12099193c7913d
Master <nyadmin@airlitelltheway.78b430.nycboe.org>
2021-06-17 12:25:02 -0400

    updated version of my Hello program

--- pre01/Hello.java
+++ pre01/Hello.java
@@ -11,11 +11,11 @@ System.out.println("Hello world");
 
 }
 
-// when done go back to the terminal and do javac Hello.java ( this is your source file)
+// when done go back to the terminal and do javac Hello.java (source file)
 // then check the content java Hello
 
 /****
-Oportunities for errors: make erors and the editor and see
+Oportunities for errors: make errors in the editor and see
 how the terminal is alerting you about it before you check the content of it
 or just javac it
 
